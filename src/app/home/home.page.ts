e96df38124f8e86d43333913284ffc6c0f239784
Francisco3104 <emiliano.s3104@gmail.com>
2024-03-17 16:17:36 -0600

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,36 @@ export class HomePage {
 
   constructor() {}
 
+  ngOnInit() {}
+
+  title: string='Mi nueva pagina';
+
+  imgUrl: string = 'https://th.bing.com/th/id/OIP.E2ZrAFvOrm_wMdE694oZCwHaEK?rs=1&pid=ImgDetMain'
+  hazmeClic(): void{
+    alert("hiciste click!");
+  }
+  verdadero: boolean = false;
+  tarjetas = [
+    {
+      nombre: 'Maria Mercedez de la Cruz',
+      numero: 8753873878786556,
+      vigenciaInicio: '08',
+      vigenciaFin: '27',
+      CVV: '088'
+    },
+    {
+      nombre: 'Arturo Alejandro Arellano',
+      numero: 972697367191209,
+      vigenciaInicio: '08',
+      vigenciaFin: '27',
+      CVV: '088'
+    },
+    {
+      nombre: 'Cesar Daniel Morales',
+      numero: 29376938983917,
+      vigenciaInicio: '08',
+      vigenciaFin: '27',
+      CVV: '088'
+    }
+  ];
 }
